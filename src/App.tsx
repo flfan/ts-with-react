a96a9aa51750100b28fb386ec68b481cbcfed673
flfan <1490459938@qq.com>
2020-06-21 15:33:19 +0800

    use hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
@@ -13,6 +13,20 @@ interface IShowResult {
   status: string;
 }
 
+interface IthemePorps {
+  [key: string] : {color: string, background: string}
+}
+
+const themes: IthemePorps = {
+  light: {color: 'red', background: 'green'},
+  dark: {color: 'blue', background: 'white'}
+}
+
+export const themeContext = React.createContext(themes.dark)
+
+
+// const style = themeContext
+
 function App() {
   const [show, setShow] = useState(true)
   const positions = useMousePosition()
@@ -20,11 +34,18 @@ function App() {
   const [refesh, setrefesh] = useState(true)
   const [data, loading] = useURLLoader(url, [refesh])
   const dogData = data as IShowResult
+  const theme = useContext(themeContext)
+  const style1 = {
+    color: theme.color,
+    background: theme.background
+  }
   return (
+    <themeContext.Provider value={themes.light}>
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3 style={{border: '1px #fff solid', cursor: 'pointer'}} onClick={(e) => {setrefesh(!refesh);e.nativeEvent.stopImmediatePropagation()}}>refesh dog</h3>
+        <h3 style={{border: '1px #fff solid', cursor: 'pointer', ...style1}} onClick={(e) => {setrefesh(!refesh);e.nativeEvent.stopImmediatePropagation()}}>refesh dog</h3>
         {loading ? <h3>loading 🐶</h3> : <img src={dogData && dogData.message} alt='dog'/>}
         <h2>X: {positions.x}, Y: {positions.y}</h2>
         <p>
@@ -44,6 +65,7 @@ function App() {
         </a>
       </header>
     </div>
+    </themeContext.Provider>
   );
 }
 
--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,16 +1,47 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react'
+import {themeContext} from '../App'
 
 const LikeButton: React.FC = () => {
   let [like, setLike] = useState(0)
   let [on, setOn] = useState(true)
+  const likeRef = useRef(0)
+  const didMountRef = useRef(false)
+  const DOMRef = useRef<HTMLInputElement>(null)
+
+  const theme = useContext(themeContext)
+  
+  const style1 = {
+    color: theme.color,
+    background: theme.background
+  }
+
+  useEffect(() => {
+    if(DOMRef && DOMRef.current){
+      DOMRef.current.focus()
+    }
+  })
+
+  useEffect(() => {
+    if(didMountRef.current){
+      console.log('this is update')
+    }else{
+      didMountRef.current = true
+    }
+  })
+  function handleAlertClick() {
+    setTimeout(() => {
+      console.log('click on ' + likeRef.current++)
+    }, 3000);
+  }
   useEffect(() => {
     document.title = `点击了${like}次~`
     console.log('click')
   }, [like])
   return (
     <>
+    <input type="text" ref={DOMRef}/>
       <button
-        style={{width: 80, height: 40, marginBottom: 20}}
+        style={{width: 80, height: 40, marginBottom: 20, ...style1}}
         onClick={(e) => {setLike(++like);e.nativeEvent.stopImmediatePropagation()}}
       >{like}
       </button>
@@ -18,6 +49,11 @@ const LikeButton: React.FC = () => {
         style={{width: 80, height: 40}}
         onClick={(e) => {setOn(!on);e.nativeEvent.stopImmediatePropagation()}}
       >{on ? "ON" : "OFF"}
+      </button>
+      <button
+        style={{width: 80, height: 40}}
+        onClick={handleAlertClick}
+      >alert
       </button>
     </>
   )
